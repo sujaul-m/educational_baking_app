445428b1602a691dc95dcfaeceda936a9949da67
Sujaul Miah <sujaul.m@gmail.com>
2019-11-04 10:00:13 +0000

    creater router.js and view files for home projects and slider.vue

---
+++ client/src/router.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import App from '@/App.vue';
+import Projects from '@/views/Projects.vue';
+import Home from '@/views/Home.vue';
+import Slider from '@/views/Slider.vue';
+
+Vue.use(Router)
+
+const router = new Router({
+
+})
+
+export default router;

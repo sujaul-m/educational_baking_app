17d4afad440c40cd39b8c8bfaefe05d86768fd10
Sujaul Miah <sujaul.m@gmail.com>
2019-11-04 19:40:33 +0000

    completed app vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,23 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <web-header/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ProjectService from './services/ProjectService.js';
+import Header from './components/Header.vue';
+import { eventBus } from './main.js';
+import ProjectCard from './components/ProjectCard.vue';
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    'web-header': Header,
+    'project-card': ProjectCard,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css">
 </style>

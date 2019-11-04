3164028dcd2d515ca616444720f400572539c974
Sujaul Miah <sujaul.m@gmail.com>
2019-11-04 08:59:30 +0000

    required in bodyPaser, MongoClient, createRouter, express and cors

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+const cors = require('cors');

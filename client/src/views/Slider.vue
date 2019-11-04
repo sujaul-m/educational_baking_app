abc4e110140affcb68c249c07aeb4780531205c3
Sujaul Miah <sujaul.m@gmail.com>
2019-11-04 20:06:34 +0000

    completed slider view

---
+++ client/src/views/Slider.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div>
+    <project-slide></project-slide>
+  </div>
+</template>
+
+<script>
+import ProjectSlide from '../components/ProjectSlide.vue';
+export default {
+  name: 'instructions',
+  components: {
+    'project-slide': ProjectSlide
+  }
+}
+</script>
+
+<style>
+</style>

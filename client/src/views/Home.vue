c6e83e0de80f37ee59162975c64acb97e96d8159
Sujaul Miah <sujaul.m@gmail.com>
2019-11-04 20:06:12 +0000

    completed home view

---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div >
+    <home-nav />
+  </div>
+</template>
+
+<script>
+import HomeNavigation from '../components/HomeNavigation.vue';
+export default {
+  name: 'home',
+  components: {
+    'home-nav': HomeNavigation
+  }
+}
+</script>
+
+<style>
+</style>

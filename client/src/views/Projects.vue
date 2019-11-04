2e0922fa89376d1218645cb6c4da5fd953cae167
Sujaul Miah <sujaul.m@gmail.com>
2019-11-04 20:06:19 +0000

    completed project view

---
+++ client/src/views/Projects.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div >
+    <project-grid />
+    <router-view id="projects"></router-view>
+  </div>
+</template>
+
+<script>
+import ProjectGrid from '../components/ProjectGrid.vue';
+export default {
+  name: 'project',
+  components: {
+    'project-grid': ProjectGrid
+  }
+}
+</script>
+
+<style>
+</style>

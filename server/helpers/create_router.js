c978eabac22526318af322101c33fb48d5772f11
Sujaul Miah <sujaul.m@gmail.com>
2019-11-06 12:36:41 +0000

    created post request

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -29,6 +29,20 @@ const createRouter = function (collection) {
     });
   });
 
+  router.post('/', (req, res) => {
+    const newData = req.body;
+    collection
+    .insertOne(newData)
+    .then((result) => {
+      res.json(result.ops[0]);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
   router.put('/:id', (req, res) => {
     const id = req.params.id;
     const updatedData = req.body;

a0fe5c1bb52742cb6854ffe1d06f83e4a2ab4f79
Sujaul Miah <sujaul.m@gmail.com>
2019-11-06 12:35:43 +0000

    created addProject to projectservice.js

--- client/src/services/ProjectService.js
+++ client/src/services/ProjectService.js
@@ -6,6 +6,17 @@ export default {
     .then(res => res.json())
   },
 
+  addProject(recipe) {
+    return fetch("http://localhost:3000/api/projects/", {
+      method: "POST",
+      body: JSON.stringify(recipe),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+  },
+
   updateProject(id, project){
     delete project._id;
     return fetch(baseURL + id, {

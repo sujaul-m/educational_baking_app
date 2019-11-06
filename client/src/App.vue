6f0a23927a587f1f57c1f5e8ddf3a5b2637f45df
Sujaul Miah <sujaul.m@gmail.com>
2019-11-06 12:36:23 +0000

    added form globally however will aim to change this in the future

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <web-header/>
     <router-view></router-view>
+    <recipe-form/>
   </div>
 </template>
 
@@ -10,11 +11,19 @@ import ProjectService from './services/ProjectService.js';
 import Header from './components/Header.vue';
 import { eventBus } from './main.js';
 import ProjectCard from './components/ProjectCard.vue';
+import RecipeForm from './components/RecipeForm.vue';
 export default {
   name: 'app',
   components: {
     'web-header': Header,
     'project-card': ProjectCard,
+    'recipe-form': RecipeForm
+  },
+  mounted(){
+    eventBus.$on('submit-recipe', (recipe) => {
+      ProjectService.addProject(recipe)
+        .then(recipeWithId => this.recipes.push(recipeWithId))
+    });
   }
 }
 </script>

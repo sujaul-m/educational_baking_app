83cec3ccd26918a9ec4bd565f9294ac5f4f58af3
Sujaul Miah <sujaul.m@gmail.com>
2020-02-13 14:31:42 +0000

    added connection to mongodb cloudserver

--- server/server.js
+++ server/server.js
@@ -1,3 +1,32 @@
+//details for local host
+// const express = require('express');
+// const app = express();
+//
+// const bodyParser = require('body-parser');
+// const MongoClient = require('mongodb').MongoClient;
+// const createRouter = require('./helpers/create_router.js');
+//
+// const cors = require('cors');
+//
+// app.use(cors());
+// app.use(bodyParser.json());
+//
+// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+//   if(err){
+//     console.log(err);
+//   }
+//   const db = client.db('baking');
+//   const projectsCollection = db.collection('projects');
+//   const projectsRouter = createRouter(projectsCollection);
+//   app.use('/api/projects', projectsRouter);
+// })
+//
+// app.listen(3000, function() {
+//   console.log(`listening on port ${this.address().port}`);
+// });
+
+
+//details for heroku
 const express = require('express');
 const app = express();
 
@@ -10,7 +39,7 @@ const cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
 
-MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+MongoClient.connect('mongodb+srv://baker_12:baker_12@bakingapp-zj8c3.mongodb.net/test?retryWrites=true&w=majority', (err, client) => {
   if(err){
     console.log(err);
   }
